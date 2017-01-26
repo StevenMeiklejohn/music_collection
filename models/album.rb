1adc7bb07fe7e78e9315f016dc2ad02280d9fb69
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-01-26 11:45:10 +0000

    final commit

--- models/album.rb
+++ models/album.rb
@@ -1,8 +1,9 @@
 require('pg')
+require_relative('../db/sql_runner')
 
 class Album
 
-  attr_reader :title, :genre, :artist_id
+  attr_accessor :title, :genre, :artist_id
 
   def initialize( options )
     @title = options['title']
@@ -15,5 +16,36 @@ class Album
     result = SqlRunner.run(sql)
     @id = result.first['id'].to_i
   end
+
+  def self.all()
+    sql = "SELECT * FROM albums;"
+    albums = SqlRunner.run(sql)
+    return albums.map { |album| Album.new( album ) }
+  end
+
+
+  def artist()
+    sql = "SELECT * FROM artists WHERE id = #{ @artist_id }"
+    artist = SqlRunner.run( sql ).first
+    result = Artist.new( artist )
+    return result
+  end
+
+  def update()
+    sql = "UPDATE albums SET (title, genre, artist_id) = ('#{@title}', '#{@genre}', #{@artist_id} ) WHERE id = #{@id};"
+    result = SqlRunner.run(sql)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM albums;"
+    result = SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM albums WHERE id =#{@id};"
+    result = SqlRunner.run(sql)
+  end
+
+
 
 end
--- models/artist.rb
+++ models/artist.rb
@@ -4,7 +4,7 @@ require_relative('../db/sql_runner')
 
 class Artist
 
-  attr_reader :id, :name
+  attr_accessor :id, :name
 
   def initialize( options )
     @id = options['id'].to_i if options['id']
@@ -18,4 +18,44 @@ class Artist
     @id = result.first['id'].to_i
   end
 
-end
+  def self.all()
+    sql = "SELECT * FROM artists;"
+    artists = SqlRunner.run(sql)
+    return artists.map { |artist| Artist.new( artist ) }
+  end
+
+
+  def albums()
+    sql = "SELECT * FROM albums WHERE artist_id = #{@id}"
+    albums = SqlRunner.run( sql )
+    result = albums.map { |album| Album.new ( album ) }
+    return result
+  end
+
+  def update()
+    sql = "UPDATE artists SET (name) = ('#{@name}') WHERE id = #{@id};"
+    result = SqlRunner.run(sql)
+  end
+
+
+  def self.delete_all()
+    sql = "DELETE FROM artists;"
+    result = SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM artists WHERE id =#{@id};"
+    result = SqlRunner.run(sql)
+  end
+
+
+  def self.find(artist_id)
+    sql = "SELECT * FROM artists WHERE id = #{artist_id}"
+    result = SqlRunner.run(sql)
+    return Artist.new( result ) 
+  end
+
+
+
+
+  end
